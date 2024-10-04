81c263bb5671a3ad76a9cab8bf5e49dfcbeb68d4
MichaelHassan-SigmaLabs <trainee.michael.hassan@sigmalabs.co.uk>
2024-10-04 13:16:04 +0100

    transform update

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -8,7 +8,7 @@ from extract import extract
 from emailing import send_email
 
 
-def transform(needs_a_clean: pd.DataFrame):
+def clean(needs_a_clean: pd.DataFrame) -> pd.DataFrame:
     """This function returns a clean dataframe ready for insertion. """
     needs_a_clean["recording_taken"] = pd.to_datetime(
         needs_a_clean["recording_taken"], format="%Y-%m-%d %H:%M:%S", errors='coerce')
@@ -22,20 +22,35 @@ def transform(needs_a_clean: pd.DataFrame):
     needs_a_clean['recording_taken'] = needs_a_clean['recording_taken'].apply(
         lambda row: row.strftime("%Y-%m-%d %H:%M:%S"))
 
-    for index, row in needs_a_clean.iterrows():
+    return needs_a_clean
+
+
+def check_conditions(clean: pd.DataFrame):
+    for index, row in clean.iterrows():
         if row['temperature'] > 50:
-            send_email(row['plant_id'], row['temperature'], 'temperature', 'exceeded')
+            send_email(row['plant_id'], row['temperature'],
+                       'temperature', 'exceeded')
 
         if row['temperature'] <= 5:
-            send_email(row['plant_id'], row['temperature'], 'temperature', 'not met')
-        
+            send_email(row['plant_id'], row['temperature'],
+                       'temperature', 'not met')
+
         if row['soil_moisture'] >= 90:
-            send_email(row['plant_id'], row['soil_moisture'], 'soil moisture', 'exceeded')
+            send_email(row['plant_id'], row['soil_moisture'],
+                       'soil moisture', 'exceeded')
 
         if row['soil_moisture'] < 30:
-            send_email(row['plant_id'], row['soil_moisture'], 'soil moisture', 'not met')
+            send_email(row['plant_id'], row['soil_moisture'],
+                       'soil moisture', 'not met')
 
-    return needs_a_clean
+
+def transform(needs_a_clean: pd.DataFrame) -> pd.DataFrame:
+    """This function returns a clean dataframe ready for insertion. """
+
+    fresh_and_clean = clean(needs_a_clean)
+    check_conditions(fresh_and_clean)
+
+    return fresh_and_clean
 
 
 if __name__ == "__main__":
