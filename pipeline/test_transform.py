9909a532a6a46c10dc73875f9d6c592a361e734d
meganlester <trainee.megan.lester@sigmalabs.co.uk>
2024-10-04 15:44:55 +0100

    tests for transforming/checking for concerning values

--- pipeline/test_transform.py
+++ pipeline/test_transform.py
@@ -1,5 +1,5 @@
 """This file tests the functionality of the transform script."""
-
+from unittest.mock import patch
 from unittest import TestCase
 
 import pandas as pd
@@ -80,3 +80,60 @@ class TestTransformFunction(TestCase):
 
         with pytest.raises(ValueError):
             clean(input_data)
+
+    @patch('transform.send_email')
+    def test_check_conditions_temperature_exceeded(self, fake_send_email):
+        """Tests that an email is sent when the temperature exceeds 50."""
+        input_data = pd.DataFrame({"plant_id": [1],
+                                   "recording_taken": ["2024-10-01 12:34:56"],
+                                   "last_watered": ["Mon, 30 Sep 2024 10:00:00 GMT"],
+                                   "soil_moisture": [45.0],
+                                   "temperature": [55.0]})
+
+        check_conditions(input_data)
+
+        # Check if send_email is called for high temperature
+        fake_send_email.assert_called_with(1, 55.0, 'temperature', 'exceeded')
+
+    @patch('transform.send_email')
+    def test_check_conditions_temperature_not_met(self, fake_send_email):
+        """Tests that an email is sent when the temperature is below or equal to 5."""
+        input_data = pd.DataFrame({"plant_id": [2],
+                                   "recording_taken": ["2024-10-02 14:20:30"],
+                                   "last_watered": ["Tue, 01 Oct 2024 11:00:00 GMT"],
+                                   "soil_moisture": [50.0],
+                                   "temperature": [3.0]})
+
+        check_conditions(input_data)
+
+        # Check if send_email is called for low temperature
+        fake_send_email.assert_called_with(2, 3.0, 'temperature', 'not met')
+
+    @patch('transform.send_email')
+    def test_check_conditions_soil_moisture_exceeded(self, fake_send_email):
+        """Tests that an email is sent when the soil moisture exceeds 90."""
+        input_data = pd.DataFrame({"plant_id": [1],
+                                   "recording_taken": ["2024-10-01 12:34:56"],
+                                   "last_watered": ["Mon, 30 Sep 2024 10:00:00 GMT"],
+                                   "soil_moisture": [95.0],
+                                   "temperature": [25.0]})
+
+        check_conditions(input_data)
+
+        # Check if send_email is called for high soil moisture
+        fake_send_email.assert_called_with(
+            1, 95.0, 'soil moisture', 'exceeded')
+
+    @patch('transform.send_email')
+    def test_check_conditions_soil_moisture_not_met(self, fake_send_email):
+        """Tests that an email is sent when the soil moisture is below 30."""
+        input_data = pd.DataFrame({"plant_id": [2],
+                                   "recording_taken": ["2024-10-02 14:20:30"],
+                                   "last_watered": ["Tue, 01 Oct 2024 11:00:00 GMT"],
+                                   "soil_moisture": [25.0],
+                                   "temperature": [22.0]})
+
+        check_conditions(input_data)
+
+        # Check if send_email is called for low soil moisture
+        fake_send_email.assert_called_with(2, 25.0, 'soil moisture', 'not met')
